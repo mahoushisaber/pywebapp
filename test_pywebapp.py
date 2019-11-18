903e7450a89a3b92a6bbe8b3e2d952adc9dcb2ae
mahoushisaber <35911839+mahoushisaber@users.noreply.github.com>
2019-11-18 14:52:18 -0800

    Update test_pywebapp.py

--- test_pywebapp.py
+++ test_pywebapp.py
@@ -6,7 +6,7 @@ b = app.browser()
 
 def test_Default():
     b.open('/')
-    assert 'COMP4913' in b.get_text()
+    assert 'A00997028' in b.get_text()
 
 def test_Api():
     b.open('/api/v1/test')
